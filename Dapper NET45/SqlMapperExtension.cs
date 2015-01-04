15b83d779102923908ae009720da9b58d533dc21
top2tech <top2tch@outlook.com>
2015-01-03 22:01:44 -0800

    add SqlMapperExtension to support one to many.

---
+++ Dapper NET45/SqlMapperExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper
+{
+    /// <summary>
+    /// Extensions for SqlMapper
+    /// </summary>
+    public static class SqlMapperExtension
+    {
+        /// <summary>
+        /// Add support for 1 to many relation.
+        /// </summary>
+        public static IEnumerable<TFirst> Map<TFirst, TSecond, TKey>
+         (
+            this Dapper.SqlMapper.GridReader reader,
+            Func<TFirst, TKey> firstKey,
+            Func<TSecond, TKey> secondKey,
+            Action<TFirst, IEnumerable<TSecond>> addChildren
+         )
+        {
+            var first = reader.Read<TFirst>().ToList();
+            var childMap = reader
+                .Read<TSecond>()
+                .GroupBy(s => secondKey(s))
+                .ToDictionary(g => g.Key, g => g.AsEnumerable());
+
+            foreach (var item in first)
+            {
+                IEnumerable<TSecond> children;
+                if (childMap.TryGetValue(firstKey(item), out children))
+                {
+                    addChildren(item, children);
+                }
+            }
+
+            return first;
+
+        }
+    }
+}
